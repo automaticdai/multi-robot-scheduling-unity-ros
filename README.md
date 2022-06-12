f842bc2147715df72b760f1c2afeb39e8f9cc828
automaticdai <automatic.dai@gmail.com>
2022-06-12 13:23:34 +0100

    init commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# multi-robot-scheduling
+# multi-robot-scheduling-unity
+Implementation of a multi-robot environment for warehouse scheduling in Unity.
+
+## Requirements
+- Unity version: 2021.3.2f1
---
+++ Unity/Assets/Scripts/agent.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class agent : MonoBehaviour
+{
+    [SerializeField] private Transform groundCheckTransform = null;
+    [SerializeField] private LayerMask playerMask;    
+    private bool jumpKeyWasPressed;
+    private float horizontalInput;
+    private Rigidbody rigidbodyComponent;
+
+
+    // Start is called before the first frame update
+    void Start() {
+        rigidbodyComponent = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            jumpKeyWasPressed = true;
+        }
+
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        rigidbodyComponent.AddForce(new Vector3(0.5f, 0f, 0f), ForceMode.VelocityChange);
+
+    }
+
+    private void FixedUpdate() {
+        rigidbodyComponent.velocity = new Vector3(horizontalInput, rigidbodyComponent.velocity.y, 0);
+    
+        // if (Physics.OverlapSphere(groundCheckTransform.position, 0.1f, playerMask).Length == 0){
+        //     return;
+        // }
+
+        if (jumpKeyWasPressed) {
+            rigidbodyComponent.AddForce(Vector3.up * 5, ForceMode.VelocityChange);
+            jumpKeyWasPressed = false;
+        }
+
+    }
+}
